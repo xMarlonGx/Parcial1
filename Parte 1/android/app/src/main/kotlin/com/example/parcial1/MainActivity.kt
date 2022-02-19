d7fc023d494906a71efa4a5e22ed118317c18428
xMarlonGx <marlonportillo41@gmail.com>
2022-02-19 17:03:32 -0600

    Borrado

--- Parte 1/android/app/src/main/kotlin/com/example/parcial1/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.parcial1
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- README.md
+++
@@ -1,3 +0,0 @@
-# Parcial1
-# Marlon Eduardo Guardado Portillo 25-0640-2017 
-# Luis Fernando Guzman Herrera 25-1451-2017
